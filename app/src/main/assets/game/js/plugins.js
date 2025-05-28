95fb9e32e4a9c9dd626ab1b2c72a77fba78fe26a
王子亮 <MrXiyi@outlook.com>
2025-05-28 18:55:33 +0800

    V1.0

--- app/src/main/assets/game/js/plugins.js
+++ app/src/main/assets/game/js/plugins.js
@@ -6,6 +6,5 @@ var $plugins =
 {"name":"XdRs_InformationTips","status":true,"description":"得失物品信息提示。","parameters":{"displayType":"R","iconTips":"false","currencyIcon":"314","loseSe":"Crossbow","variableSe":"Chime2","goldSe":"Shop2","itemSe":"Equip1","color0":"11","color1":"18","variableArr":"[]"}},
 {"name":"XdRs_BubbleDialog","status":true,"description":"气泡对话框。","parameters":{}},
 {"name":"XdRs_AutomaticSpeaking","status":true,"description":"NPC自动说话 + 指定对象说话 （MV + MZ）。","parameters":{"windowskins":"","defaultDuration":"180","defaultWaitCount":" 360"}},
-{"name":"CharacterInfoWindow","status":true,"description":"创建一个显示角色名称和等级信息的窗口，并提供打开和关闭窗口的插件指令","parameters":{}},
 {"name":"Fun_Actor_Title_Show","status":true,"description":"地图上主角头顶显示称号。","parameters":{}}
 ];
